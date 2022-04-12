8fef619da22756bd3bf62944a242d7b694a1abcb
timeisdivine <grgnbura@yahoo.com.tr>
2022-04-12 04:51:02 +0300

    spider yazılmaya başlandı

---
+++ worldometers/spiders/countries.py
@@ -0,0 +1,15 @@
+import scrapy
+
+class CountriesSpider(scrapy.Spider):
+    name = 'countries'
+    allowed_domains = ['www.worldometers.info/world-population']
+    start_urls = ['http://www.worldometers.info/world-population/population-by-country//']
+
+    def parse(self, response):
+        title = response.xpath("//h1/text()").get()
+        countries = response.xpath("//td/a/text()").getall()
+
+        yield {
+            'title': title,
+            'countries': countries,
+        }
